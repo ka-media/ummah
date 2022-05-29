7bd99322068cb6adaa3e643612c0f8aa2ff4e462
Muhammad Kamel Umar <72482538+ka-media@users.noreply.github.com>
2022-05-29 12:35:08 +0100

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,66 +1,23 @@
 
-  /*const firebaseConfig = {
-     apiKey: "AIzaSyBd5ATAul4UIOgAsmXHIQGVAe3XqK6Xcps",
-     authDomain: "centralzonewebapp.firebaseapp.com",
-     projectId: "centralzonewebapp",
-     storageBucket: "centralzonewebapp.appspot.com",
-     messagingSenderId: "803107610265",
-     appId: "1:803107610265:web:84145ab38511ebcc10e208",
-     measurementId: "G-MKB4FZCP60"
-   };
- 
-   // Initialize Firebase
- 
-   firebase.initializeApp(firebaseConfig);
- 
-   firebase.analytics();
- 
+  const removeMenu = document.querySelector('#icon');
+  const logo = document.querySelector('#logo');
+  const addNav = document.querySelector('#flex-nav');
+  const nav = document.querySelector('#nav');
 
-  firebase.auth.Auth.Persistence.LOCAL;
+  const menu = document.getElementById('icon');
 
-   
+  menu.addEventListener('click', function() {
+    
+    const removeMenu = document.getElementById('icon').remove();
+    const logo = document.getElementById('logo').remove();
 
-  $("#btn-login").click(function() {
+    const addNav = document.getElementById('flex-nav');
+    addNav.classList.remove('d-none');
 
-       //var userName = $("userName").val();
-       var email = $("#email").val();
-       var password = $("#password").val();
-
-       if(email != " " && password != " ") {
-
-          var result = firebase.auth().signInWithEmailAndPassword(email, password);
-
-          result.catch(function(error) {
-
-               //var errorCode = error.code;
-               var errorMessage = error.message;
-               alert("message : " + errorMessage);
-
-          }); 
-     }
-            else {
-            alert("Please enter valid information!");
-       }
+    
   });
 
-  function showMessage(nice) {
-     document.getElementById('nice').innerHTML = nice;
-  }
-
-  showMessage('nice');*/
-
-  const removeMenu = document.querySelector('#icon');
-        const addNav = document.querySelector('#flex-nav');
-        const nav = document.querySelector('#nav');
-
-        const menu = document.getElementById('icon');
-        menu.addEventListener('click', function() {
-          
-          const removeMenu = document.getElementById('icon').remove();
-          const addNav = document.getElementById('flex-nav');
-
-          addNav.classList.remove('d-none');
-          
-        });
+  /*Contact form script 
+   */
 
  
